198cabe0ca91fb41d647987937907af792b3437d
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-03-04 20:55:56 +0100

    navbar add shop-icon

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,11 +1,9 @@
 /* Add switching scrollbar */
 
 const nav = document.getElementById(`nav-d`);
-const sectionOne = document.getElementsByTagName(`header`);
 const navlogo = document.querySelector(`.nav--logo`);
 const navLinks = document.querySelectorAll(`.nav--links a`);
 const header = document.querySelector(`header`);
-const burgerLogo = document.getElementById(`left4`);
 
 const sectionOneOptions = {
   rootMargin: "-65px 0px 0px 0px",
@@ -16,22 +14,21 @@ const sectionOneObserver = new IntersectionObserver(function (
   observer
 ) {
   entries.forEach((entry) => {
-      console.log(entry.isIntersecting);
+    console.log(entry.isIntersecting);
     if (!entry.isIntersecting) {
       navLinks.forEach((navLink) => {
         nav.classList.add("whiteBg");
-        navLink.classList.add("changeColor");
-        navlogo.classList.add("changeColor");
+        navLink.classList.add("invertColor");
+        navlogo.classList.add("invertColor");
       });
     } else {
-        navLinks.forEach((navLink) => {
-            nav.classList.remove("whiteBg");
-            navLink.classList.remove("changeColor");
-            navlogo.classList.remove("changeColor");
-          });
+      navLinks.forEach((navLink) => {
+        nav.classList.remove("whiteBg");
+        navLink.classList.remove("invertColor");
+        navlogo.classList.remove("invertColor");
+      });
     }
   });
 });
 
 sectionOneObserver.observe(header);
-
