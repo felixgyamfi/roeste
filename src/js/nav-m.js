4ea26106392996ae3ff18697cb7b32bf2283e234
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-03-05 00:41:35 +0100

    navbar-m add color switch

--- src/js/nav-m.js
+++ src/js/nav-m.js
@@ -1,6 +1,7 @@
+// toggle function for menu slide
 function toggleVisible() {
-    const menu = document.getElementById(`nav--menu`);
-    menu.classList.toggle(`visible`)
+  const menu = document.getElementById(`nav--menu`);
+  menu.classList.toggle(`visible`);
 }
 
 const elOpen = document.getElementById(`nav--burger`);
@@ -8,5 +9,42 @@ elOpen.addEventListener("click", toggleVisible);
 const elClose = document.getElementById(`menu-close`);
 elClose.addEventListener("click", toggleVisible);
 
+// color switch fuction for navbar-m
 
-console.log(`nav-m executed`)
+//const navm = document.getElementById(`nav-m`);
+//const navlogo = document.querySelector(`.nav--logo-m`);
+//const navBurger = document.getElementById(`nav--burger`);
+
+// const header = document.querySelector(`header`);
+// const options = {};
+// const observer = new IntersectionObserver(function(entries, observer) {
+//     entries.forEach(entry => {
+//         console.log(entry);
+//     })
+// }, options);
+// observer.observe(header);
+
+const nav = document.getElementById(`nav-m-container`);
+const navlogo = document.getElementById(`nav--logo-m`);
+const navBurger = document.getElementById(`nav--burger`);
+const header = document.querySelector(`header`);
+
+const sectionOneObserver = new IntersectionObserver(function (
+    entries,
+    observer
+  ) {
+    entries.forEach((entry) => {
+      console.log(entry.isIntersecting);
+      if (!entry.isIntersecting) {
+        nav.classList.add("whiteBg");
+          navBurger.classList.add("invertColor");
+          navlogo.classList.add("invertColor");
+      } else {
+        nav.classList.remove("whiteBg");
+          navBurger.classList.remove("invertColor");
+          navlogo.classList.remove("invertColor");
+      }
+    });
+  });
+  
+  sectionOneObserver.observe(header);
