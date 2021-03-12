f66c05b06d7d3c29f6333e4fdd43050b688f8ea7
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-03-12 20:50:56 +0100

    build _shop, _cart html&css, build shop JS (unfinished)

--- src/js/shop.js
+++ src/js/shop.js
@@ -7,10 +7,11 @@ function shop() {
   const productsHTMLArray = products
     .map(
       (product) => `
-    <div class="display--product-container">
-    <img class="display--product-img" src="${
+    <div class="display--product-container" data-id="${product.id}">
+    <div class="display--img-container"><div class="quick-add-btn">quick add +</div><img class="display--product-img" src="${
       productImages[product.image.imageName]
     }" alt="${product.image.alt}"></img>
+    </div>
     <div class="display--info-container">
       <div class="product-name">${product.productName}</div>
       <div class="product-price">${product.price / 100}0 €</div>
@@ -34,3 +35,18 @@ function shop() {
 }
 
 export default shop;
+
+// quick add button hover animation
+
+function hoverQuickAdd() {
+  const productContainer = document.querySelectorAll(".display--product-container");
+  var quickAdd = document.querySelector(".quick-add-btn");
+
+  productContainer.addEventListener("mouseover", hoverIn, false);
+  productContainer.addEventListener("mouseout", hoverOut, false);
+
+  productContainer.setAttribute("style", "background-color:blue;")
+
+}
+
+hoverQuickAdd();
